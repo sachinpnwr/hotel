b949a5a0871e35882bc82e68a8eadaca3c40858d
Sachin <wwwsachinsingh815@gmail.com>
2023-02-28 18:21:44 +0530

    slider

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let hamburger = document.querySelector('.hamburger')
 let navItems = document.querySelector('#navItems')
 let navLink = document.querySelectorAll('.nav-link')
+let slides = document.querySelectorAll('.slides')
+let currentSlide = 0;
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
@@ -11,3 +13,19 @@ navLink.forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navItems.classList.remove('active');
 }))
+
+function changeSlide(moveTo){
+    if (moveTo >= slides.length){
+        moveTo = 0;
+    }
+    if (moveTo < 0 ){
+        moveTo = slides.length-1;
+    }
+    slides[currentSlide].classList.toggle('active');
+    slides[moveTo].classList.toggle('active');
+    currentSlide = moveTo;
+}
+
+window.onload = setInterval(()=>{
+    changeSlide(currentSlide+1);
+},3000)
